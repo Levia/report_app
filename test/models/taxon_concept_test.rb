ed0e0e6252b10852d8bdfa8f8f931fbb516f0786
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-05-07 14:42:57 +0100

    Add taxon concept test

---
+++ test/models/taxon_concept_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class TaxonConceptTest < ActiveSupport::TestCase
+  test "invalid when already existing scientific_name" do
+    tc = TaxonConcept.create({scientific_name: 'Canis Lupus'})
+    tc2 = TaxonConcept.new({scientific_name: 'Canis Lupus'})
+
+    assert !tc2.valid?, "Scientific name has already been taken"
+  end
+end
