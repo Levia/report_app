8e1c4bd589618acec688f943d448c2f4a01f2b69
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-05-07 15:44:57 +0100

    Add user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should respond to reports" do
+    user = users(:ferdinando)
+
+    assert_respond_to user, :reports
+  end
 end
